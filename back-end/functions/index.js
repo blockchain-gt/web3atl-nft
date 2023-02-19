f5fb052431dae9c398177f2d52273120aa979183
thanhtrinh2003 <thanhtrinh280506@gmail.com>
2023-02-19 14:39:13 -0500

    update functions

--- back-end/functions/index.js
+++ back-end/functions/index.js
@@ -20,23 +20,90 @@ admin.initializeApp();
 // Take the text parameter passed to this HTTP endpoint and insert it into 
 // Firestore under the path /messages/:documentId/original
 exports.addEmail = functions.https.onRequest(async (req, res) => {
-    const original = req.query.text.toLowerCase();
-    let textArray = original.split(",")
-    let email = textArray[1]
-    let type = textArray[2]
-
+    const original = req.query.param;
+    console.log(original);
+    let email = original[0].toLowerCase();
+    let type = original[1].toLowerCase();
 
     const db = admin.firestore();
     const doc = db.collection('Email').doc("Attendance List").get();
     let data = (await doc).data();
-    
-    data[type].push(email);
+     
+    data[type].push(email+",");
     db.collection('Email').doc("Attendance List").set(data);
 
     res.send(original + " email has been added to " + type + " email list!");
 });
 
 
+exports.verifyEmail = functions.https.onRequest(async (req, res) => {
+    const original = req.query.param;
+    console.log(original);
+    let email = original[0].toLowerCase();
+    let type = original[1].toLowerCase();
+    let address = original[2].toLowerCase();
+ 
+    const db = admin.firestore();
+    const doc = db.collection('Email').doc("Attendance List").get();
+    let data = (await doc).data();
+
+    console.log(type)
+
+    let list = data[type]
+    let size = list.length
+    
+    for (let i = 0; i<size; i++) {
+        let currentString = list[i]
+        console.log(currentString)
+        let components = currentString.split(",")
+        let currentEmail = components[0]
+        if (currentEmail.localeCompare(email)==0) {
+            list[i] = currentEmail+","+address;
+            data[type] = list
+            db.collection('Email').doc("Attendance List").set(data)
+        } 
+    }
+});
+
+
+exports.updateAddress = functions.https.onRequest(async (req, res) => {
+    const original = req.query.param;
+    console.log(original);
+    let email = original[0].toLowerCase();
+    let type = original[1].toLowerCase();
+    let address = original[2].toLowerCase();
+ 
+    const db = admin.firestore();
+    const doc = db.collection('Email').doc("Attendance List").get();
+    let data = (await doc).data();
+
+    console.log(type)
+
+    let list = data[type]
+    let size = list.length
+    
+    for (let i = 0; i<size; i++) {
+        let currentString = list[i]
+        console.log(currentString)
+        let components = currentString.split(",")
+        let currentEmail = components[0]
+        if (currentEmail.localeCompare(email)==0) {
+            list[i] = currentEmail+","+address;
+            data[type] = list
+            db.collection('Email').doc("Attendance List").set(data)
+            res.send("Email Verified");
+        } 
+    }
+    res.send("Email has not been registered!");
+});
+
+
+exports.queryList = functions.https.onRequest(async (req, res) => {
+    const db = admin.firestore();
+    const doc = db.collection('Email').doc("Attendance List").get();
+    let data = (await doc).data();
+    res.json({"general": data["general"], "speaker": data["speaker"], "hacker": data["hacker"], "team": data["team"]})
+});
 
 
 // Take the text parameter passed to this HTTP endpoint and insert it into 
